54d43118764c4b7de12f046cfdfd34e7c5fa9898
Edmond <pefouho-edmond.nguefeu>
2022-11-24 20:29:50 +0100

    ee

---
+++ selection_sort_file_meta/my_sort.hpp
@@ -0,0 +1,37 @@
+// Change me instead
+// All occurences of XXX need to be replaced with
+// something more meaningful
+
+
+
+//void swap(int *xp, int *yp)
+////{
+    //int temp = *xp;
+    //*xp = *yp;
+    //*yp = temp;
+//}
+
+
+template <typename T>
+void my_selection_sort(T begin, T end) {
+    T cp = begin;
+    while (cp != end)
+    {
+        T min = cp;
+        T sto = cp;
+        while (sto != end)
+        {
+            if (*min > *sto)
+                min = sto;
+            sto++;
+
+        }
+        
+        auto check = *cp;
+        *cp = *min;
+        *min = check;
+        cp++;
+    }
+
+
+}
